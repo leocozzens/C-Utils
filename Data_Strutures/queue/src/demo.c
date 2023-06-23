fac69ee7de99d5e0239fe4af09588b00aa5dae49
Leo Cozzens <leo@narthexnetworks.com>
2023-06-23 10:44:45 -0500

    Segmented make
    Added makefiles to each segment
    Made demo for queue more robust

--- Data_Strutures/queue/src/demo.c
+++ Data_Strutures/queue/src/demo.c
@@ -1,7 +1,7 @@
 #include <queue.h>
 #include <time.h>
 
-#define Q_COUNT 3
+#define Q_COUNT 10
 
 int main(void) {
     int out;
@@ -11,15 +11,17 @@ int main(void) {
     for(int i = 0; i < Q_COUNT; i++) make_queue(&q[i]);
 
     for(int qIndex = 0; qIndex < Q_COUNT; qIndex++) {
-        printf("\nQueue index: %d\n", qIndex + 1);
-        for(int i = 10; i <= 100; i += 10) {
-            enqueue(&q[qIndex], rand() % (i + 1));
-        }
+            for(int i = 0; i < 2; i++) {
+            printf("\nQueue index: %d\nTry: %d\n", qIndex + 1, i + 1);
+            for(int j = 10; j <= 100; j += 10) {
+                enqueue(&q[qIndex], rand() % (j + 1));
+            }
 
-        while(dequeue(&q[qIndex], &out)) {
-            printf("out = %d\n", out);
+            while(dequeue(&q[qIndex], &out)) {
+                printf("out = %d\n", out);
+            }
+            if(q[qIndex].head == NULL && q[qIndex].tail == NULL) printf("Empty!\n");
         }
-        if(q[qIndex].head == NULL && q[qIndex].tail == NULL) printf("Empty!\n");
     }
     return 0;
 }
